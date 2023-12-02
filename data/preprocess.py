b7c500cc62e201c3ff880e9533a40746442b215a
Timofey Sedov <tima.sedov.2004@mail.ru>
2023-12-02 12:51:47 +0300

    finish sar

--- data/preprocess.py
+++ data/preprocess.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 from recommenders.datasets.python_splitters import python_stratified_split
+import os
 
 np.random.seed(42)
 
@@ -18,6 +19,8 @@ train, test = python_stratified_split(
     data, ratio=0.75, col_user="user_id", col_item="item_id", seed=42
 )
 
+if not os.path.exists("../data/interim/ml-100k"):
+    os.makedirs("../data/interim/ml-100k")
 
-train.to_csv("../data/interim/train.csv", index=False)
-test.to_csv("../data/interim/test.csv", index=False)
+train.to_csv("../data/interim/ml-100k/train.csv", index=False)
+test.to_csv("../data/interim/ml-100k/test.csv", index=False)
